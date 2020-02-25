a3bc8585720c4d01934b84b90f67bed7812a3ed3
shadz22 <shadz22@yahoo.co.uk>
2020-02-25 21:47:50 +0000

    Create the Printer class

---
+++ lib/printer.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Printer
+
+  def print_transactions(statement)
+  end
+end
---
+++ spec/printer_spec.rb
@@ -0,0 +1,6 @@
+require 'printer.rb'
+
+describe Printer do
+  
+  it { is_expected.to respond_to(:print_transactions).with(1).argument }
+end
