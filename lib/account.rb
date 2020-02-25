4bfdbebed9852ef861385e9eb96d2b00b3ca4155
shadz22 <shadz22@yahoo.co.uk>
2020-02-25 10:17:29 +0000

    Deposit increases the balance by the amount deposited

--- lib/account.rb
+++ lib/account.rb
@@ -6,4 +6,8 @@ class Account
     @balance = 0
     @statement =[]
   end
+
+  def deposit(amount)
+    @balance += amount
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,14 @@ describe Account do
   it 'has a statement' do
     expect(subject.statement).to be_an_instance_of(Array)
   end
+
+  it {is_expected.to respond_to(:deposit).with(1).argument }
+
+  describe '#deposit' do
+    it 'increases the balance by the amount deposited' do
+      subject.deposit(10)
+      expect(subject.balance).to eq 10
+    end
+  end
+
 end
