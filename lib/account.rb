421f00dd408e08261d1bfa380c48475841846919
shadz22 <shadz22@yahoo.co.uk>
2020-02-25 21:29:41 +0000

    Create private methods for updating balance after transactions

--- lib/account.rb
+++ lib/account.rb
@@ -11,14 +11,22 @@ class Account
   def deposit(amount)
     raise 'Deposit amount must be a number' unless amount.is_a? Numeric
     raise 'Deposit must be a positive number' if amount <= 0
-
-    @balance += amount
+    make_deposit(amount)
   end
 
   def withdraw(amount)
     raise 'Withdraw amount must be a number' unless amount.is_a? Numeric
     raise 'Withdraw must be a positive number' if amount <= 0
+    make_withdraw(amount)
+  end
 
+  private
+  def make_deposit(amount)
+    @balance += amount
+  end
+
+  def make_withdraw(amount)
     @balance -= amount
   end
+  
 end
